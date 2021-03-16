922b4a542d594609daad9bc37ed54d781d42f94c
ArjanKamphuis <arjan_kamphuis@hotmail.com>
2021-03-16 23:59:19 +0100

    003 - A Thread Can Have Replies

---
+++ tests/Unit/ReplyTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Reply;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ReplyTest extends TestCase
+{
+    use RefreshDatabase;
+    
+    /** @test */
+    public function it_has_an_owner()
+    {
+        $reply = Reply::factory()->create();
+        $this->assertInstanceOf('App\Models\User', $reply->owner);
+    }
+}
