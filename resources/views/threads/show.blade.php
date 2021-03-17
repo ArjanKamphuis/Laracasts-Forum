0bf0549425487fe1616970ecec600a6ee6eb4235
ArjanKamphuis <arjan_kamphuis@hotmail.com>
2021-03-17 16:35:15 +0100

    005 - The Reply Form

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -19,5 +19,21 @@
             @endforeach
         </div>
     </div>
+
+    @if (auth()->check())
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <form method="POST" action="{{ $thread->path() . '/replies' }}">
+                    @csrf
+                    <div class="form-group">
+                        <textarea name="body" id="body" class="form-control" rows="5" placeholder="Have something to say?"></textarea>
+                    </div>
+                    <button class="btn btn-outline-primary" type="submit">Post</button>
+                </form>
+            </div>
+        </div>
+    @else
+        <p class="text-center">Please <a href="{{ route('login') }}">sign in</a> to participate in this discussion.</p>
+    @endif
 </div>
 @endsection
